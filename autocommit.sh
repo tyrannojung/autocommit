0d35cfc76e1754c09944f742f57178e4d93e4ac6
jungdawoon <tyrannojung@abc.ne.kr>
2021-12-22 11:07:51 +0900

    Day

--- autocommit.sh
+++ autocommit.sh
@@ -11,7 +11,7 @@ if [  "$?" = "0" ]; then
 		echo "1" >> $Dir/$FILE
 	fi
 
-	sed -i "s/$(cat $Dir/$FILE)/`expr $(cat $Dir/$FILE) + 1`/g" $Dir/$FILE
+	sed -i "1s/.*/`expr $(cat $Dir/$FILE) + 1`/g" $Dir/$FILE
 
 	cd $Dir
 	git add .
